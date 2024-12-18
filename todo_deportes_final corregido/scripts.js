842fadac3fbef1e22dc4cddb2d918f216e23a4b4
Ale2213 <jandroale666@gmail.com>
2024-12-17 22:18:05 -0300

    Add files via upload

---
+++ todo_deportes_final corregido/scripts.js
@@ -0,0 +1,6 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    darkModeToggle.textContent = document.body.classList.contains('dark-mode') ? '☀️ Modo Claro' : '🌙 Modo Oscuro';
+});
